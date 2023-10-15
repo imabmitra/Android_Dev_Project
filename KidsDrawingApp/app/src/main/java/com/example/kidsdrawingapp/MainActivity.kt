7b8ef1c8d1084d2d69217d15b0735bbd0c5936d3
Abhishek Kumar <abmitra222000@gmail.com>
2023-10-15 15:12:32 +0530

    feature added in kids drawing app

--- KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ KidsDrawingApp/app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -5,26 +5,50 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     var DrawView:DrawingView?= null
     var undo:ImageButton?=null
     var eraser:ImageButton?=null
+    var red:TextView?=null
+    var black:TextView?=null
+    var green:TextView?=null
+    var yellow:TextView?=null
+    var blue:TextView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         DrawView = findViewById(R.id.drawing_view)
         undo =findViewById<ImageButton>(R.id.undo)
         eraser = findViewById<ImageButton>(R.id.Eraser)
+        red = findViewById<TextView>(R.id.red)
+        black = findViewById<TextView>(R.id.black)
+        green = findViewById<TextView>(R.id.green)
+        yellow = findViewById<TextView>(R.id.yellow)
+        blue = findViewById<TextView>(R.id.blue)
         undo?.setOnClickListener {
-        DrawView?.undo()
+            DrawView?.undo()
 
         }
         eraser?.setOnClickListener {
             DrawView?.selectmarker(Color.WHITE)
         }
-
-
+        red?.setOnClickListener {
+            DrawView?.selectmarker(Color.RED)
+        }
+        black?.setOnClickListener {
+            DrawView?.selectmarker(Color.BLACK)
+        }
+        green?.setOnClickListener {
+            DrawView?.selectmarker(Color.GREEN)
+        }
+        yellow?.setOnClickListener {
+            DrawView?.selectmarker(Color.YELLOW)
+        }
+        blue?.setOnClickListener {
+            DrawView?.selectmarker(Color.CYAN)
+        }
     }
 }
